56e449c7c1c847dc3ec15e285df7b3a231f8687c
Muhammed2024Salama <devmuhammedsalama@gmail.com>
2025-07-19 01:16:37 +0300

    Partial Resource Route

--- routes/web.php
+++ routes/web.php
@@ -39,4 +39,5 @@ Route::get('/printName/{name?}', [\App\Http\Controllers\TestController::class,'p
 Route::get('/hello',[\App\Http\Controllers\UserController::class,'index']);
 Route::get('/hello1',[\App\Http\Controllers\Users\UserController::class,'index']);
 Route::get('/hello2',\App\Http\Controllers\SingleController::class);
-Route::resource('/posts',\App\Http\Controllers\PostController::class);
+//Route::resource('/posts',\App\Http\Controllers\PostController::class)->only(['index','show']);
+Route::resource('/posts',\App\Http\Controllers\PostController::class)->except(['index','show']);
