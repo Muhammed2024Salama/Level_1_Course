e7ea3ad4c53af888a0895b4881ed9b6006b8e682
Muhammed2024Salama <devmuhammedsalama@gmail.com>
2025-07-18 18:49:29 +0300

    Shorthand Routes

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,9 @@ Route::get('/', function () {
 });
 
 Route::get('/mohamed', [\App\Http\Controllers\TestController::class,'mohamed']);
-
+//Route::view('/mohamed','index');
+//Route::redirect('/here','/there');
+//Route::redirect('/there','https://github.com/Muhammed2024Salama');
 Route::get('/ahmed',function (){
     return "Hello Ahmed Salama";
 });
